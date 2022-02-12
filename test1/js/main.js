bf384557fd157dfad3fa3feb6a2f4dfbb4971b31
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-02-12 20:55:04 +0900

    テキストをロードするやつはモジュールにすることができた、でもアニメーションもローディングもテキストに関係するものだからテキストクラスとか一つのクラスに格納した方がいいと思う。  #1

--- test1/js/main.js
+++ test1/js/main.js
@@ -1,64 +1,47 @@
 "use strict";
 import TextAnimation  from './lib/TextAnimation.js';
+import TextLoading from './lib/TextLoading.js';
 
 (function(){
 
-    window.addEventListener('load',(e)=>{
+    let gameState = {
+        "title":true,
+        "textloading":false,
+        "msgindex":0
+    }
 
-        let gameState = {
-            "title":true,
-            "textloading":false,
-            "msgindex":0
-        }
-        let msgs = [
-            "今日のご飯は",
-            "焼肉に行こうと思ったがお金がなかったので、自炊することにした。",
-            "冷蔵庫を見た。",
-            "何も食べれそうなものがなかった。あったのは調味料とビールだけ…。",
-            "「こりゃ、ダメだな...笑」",
-            "俺はそうつぶやいて、近所のスーパーへ出かけること決意した。",
-            "顔を洗い、髭を剃り、歯を磨いて、…。それから着替え、身支度を済ませてドアを開けた。"
-        ]
-        function TextLoading() {
-            if (gameState.msgindex>=msgs.length) {
-                alert('終了');
-                return;
-            }
-            const msgfragment = document.createDocumentFragment();
-            for (let i = 0; i < msgs[gameState.msgindex].length; i++) {
-                const element = msgs[gameState.msgindex][i];
-                const span = document.createElement('span');
-                span.innerText=element;
-                span.className='op0';
-                msgfragment.appendChild(span);
-            }
-            document.getElementById('dialogue').innerHTML='';
-            document.getElementById('dialogue').appendChild(msgfragment);
-            gameState.msgindex++;
-    
-        }
+    let msgs = [ //コンストラクタに入れる
+        "今日のご飯は",
+        "焼肉に行こうと思ったがお金がなかったので、自炊することにした。",
+        "冷蔵庫を見た。",
+        "何も食べれそうなものがなかった。あったのは調味料とビールだけ…。",
+        "「こりゃ、ダメだな...笑」",
+        "俺はそうつぶやいて、近所のスーパーへ出かけること決意した。",
+        "顔を洗い、髭を剃り、歯を磨いて、…。それから着替え、身支度を済ませてドアを開けた。"
+    ]
 
-        const screen = document.getElementById('screen');
-        screen.addEventListener('click',(e)=>{
+    let TextData = new TextLoading(msgs,gameState)
 
-            if (gameState.title) {
-                gameState.title=false;
-                document.querySelector('#screen .msg-txt').classList.add('none');
-                TextLoading();
+    const screen = document.getElementById('screen');
+    screen.addEventListener('click',(e)=>{
 
-            }else{
+        if (gameState.title) {
+            gameState.title=false;
+            document.querySelector('#screen .msg-txt').classList.add('none');
+            TextData.Loading();
 
-                let text = document.querySelectorAll('#dialogue .op0');
-                if (text.length==0) {
-                    TextLoading();
-                    // console.log(text);
-                    text = document.querySelectorAll('#dialogue .op0');
-                }
-                const AnimeC = new TextAnimation(text)
+        }else{
+
+            let text = document.querySelectorAll('#dialogue .op0');
+            if (text.length==0) {
+                TextData.Loading();
                 // console.log(text);
-                AnimeC.AnimationStart(text);
+                text = document.querySelectorAll('#dialogue .op0');
             }
-        
-        })
+            const AnimeC = new TextAnimation(text)
+            // console.log(text);
+            AnimeC.AnimationStart(text);
+        }
+    
     })
 })();
