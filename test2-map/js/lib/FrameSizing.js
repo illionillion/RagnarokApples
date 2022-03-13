3fc91cf545a7211d5f61cc2c01c9a4ddc871f3c4
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-03-13 14:50:40 +0900

    マップ画面を拡大させた。これから選択後のポップアップ作る #4

---
+++ test2-map/js/lib/FrameSizing.js
@@ -0,0 +1,13 @@
+export default function FrameSizing(){
+    // console.log('size');
+
+    //PC画面の幅を取得してから16:9で計算
+    //縦のサイズを割り当てる
+
+    let ScreenWidth = window.innerWidth
+    console.log(ScreenWidth);
+    console.log(document.getElementById('frame').style);
+    let ScreenHeight = 9 * ScreenWidth / 16
+    console.log(ScreenHeight);
+    document.getElementById('frame').style.height=`${ScreenHeight}px`
+}
--- test2-map/js/main.js
+++ test2-map/js/main.js
@@ -1,9 +1,13 @@
 "use strict";
+import FrameSizing from "./lib/FrameSizing.js";
 
 (function(){
     // console.log(document.getElementById('screen'));
+    window.addEventListener('resize',(e)=>{
+        FrameSizing()
+    })
     window.addEventListener('load',(e)=>{
-
+        FrameSizing()
         const point_eles = document.getElementsByClassName('map-touch')
         // console.log(point_eles);
         for (const point of point_eles) {
