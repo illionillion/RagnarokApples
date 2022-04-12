cdabc8f59f0a5db203d22f7b3207a15fa38a9c5d
yuto ichinose <yuto.0526.sano@gmail.com>
2022-04-12 13:59:20 +0900

    コメントアウト追加

--- test3-prototype-April/js/lib/FrameSizing.js
+++ test3-prototype-April/js/lib/FrameSizing.js
@@ -8,6 +8,8 @@ export default function FrameSizing(){
     console.log(ScreenWidth);
     console.log(document.getElementById('frame').style);
     let ScreenHeight = 9 * ScreenWidth / 16
+    // let ScreenHeight = 3 * ScreenWidth / 4
+    // let ScreenHeight = 10 * ScreenWidth / 16
     console.log(ScreenHeight);
     document.getElementById('frame').style.height=`${ScreenHeight}px`
 }
