c91569d7257e30f7808735490a61c3ce045b2e2f
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-05-26 23:48:33 +0900

    ファイルはダウンロードできていない？空のファイルが作られて上書きされる

--- main.js
+++ main.js
@@ -18,41 +18,51 @@ const createWindow = () =>{
         // titleBarStyle: 'hidden'
     })
     
-    // // ここで開く前にレンダラーのasarをサーバーから差し替え
-    // if (!app.isPackaged) {
-    //     // モジュールロード
-    //     const http = require('http');
-    //     const fs = require('fs');
+    // ここで開く前にレンダラーのasarをサーバーから差し替え
+    if (!app.isPackaged) {
+        // モジュールロード
+        // const http = require('http');
+        // const fs = require('fs');
+        const http = require('https');
+        const fs = require('original-fs');
 
-    //     // URLを指定 
-    //     const url = __dirname + '/render.asar';
+        // (async ()=>{
+            
+            // URLを指定 
+            // const url = __dirname + '/render.asar';
+            const url = 'https://ji9xputuw8gwgczk2gnxzg.on.drv.tw/www.render.asar.server.com/' + 'render.asar';
+            // const url = 'http://localhost:8080/' + 'render.asar';
+            
+            // 出力ファイル名を指定
+            const outFile = fs.createWriteStream(app.getPath('userData') + '/render.asar');
+            // const outFile = fs.createWriteStream('./rendertest.asar');
+            
+            // ダウンロード開始
+            const req = http.get(url, function (res) {
+                
+                // ダウンロードした内容をそのまま、ファイル書き出し。
+                res.pipe(outFile);
+                
+                // 終わったらファイルストリームをクローズ。
+                res.on('end', function () {
+                    outFile.close();
+                }); 
+            });
 
-    //     // 出力ファイル名を指定
-    //     const outFile = fs.createWriteStream(app.getPath('userData') + 'render.asar');
-
-    //     // ダウンロード開始
-    //     const req = http.get(url, function (res) {
-
-    //         // ダウンロードした内容をそのまま、ファイル書き出し。
-    //         res.pipe(outFile);
-
-    //         // 終わったらファイルストリームをクローズ。
-    //         res.on('end', function () {
-    //             outFile.close();
-    //         }); 
-    //     });
-
-    //     // エラーがあれば扱う。
-    //     req.on('error', function (err) {
-    //         console.log('Error: ', err); return;
-    //     });
-    // }
+            // エラーがあれば扱う。
+            req.on('error', function (err) {
+                console.log('Error: ', err); return;
+            });
+        // })()
+    }
 
     // ファイルを開く
+    // //C:\Users\user\AppData\Roaming\RA
     if (app.isPackaged) {//アプリがパッケージングされてる場合
         win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
     }else{
-        win.loadURL(__dirname + '/render.asar/index.html') //asarの中のアプリを開く
+        win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
+        // win.loadURL(__dirname + '/render.asar/index.html') //asarの中のアプリを開く
     }
     
     // if (app.isPackaged) {
