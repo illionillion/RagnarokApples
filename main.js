179e9c506c64178c670aa11685ba433cadafde7b
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-06-02 22:07:07 +0900

    起動時の画面遷移を変更 スカッシュ->ダウンロード開始->終了->メインウィンドウ

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const { app, Menu, BrowserWindow } = require('electron')
 const https = require('https')
 const originalfs = require('original-fs');
 const request = require('request');
+// const fetch = require('node-fetch')
 
 /**
  * ウィンドウを作成する
@@ -21,65 +22,14 @@ const createWindow = () =>{
         // frame: false ,
         // titleBarStyle: 'hidden'
     })
-    // if (app.isPackaged) {//アプリがパッケージングされてる場合
-        // ここで開く前にレンダラーのasarをサーバーから差し替え
-        // URLを指定 
-        const url = 'https://drive.google.com/uc?id=1mHfOd4seMjFuknv6hB1C55U9kMiw64sN&confirm=t';
-        
-        // 出力ファイル名を指定
-        const outFile = originalfs.createWriteStream(app.getPath('userData') + '/render.asar');
-        let splashWin
-        // const outFile = originalfs.createWriteStream('./rendertest.asar');
+    win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
 
-        // request使わずにダウンロード
-        // const req = https.request(url, (res) => {
-        //     // res.pipe(outFile)
-        //     res.on('data', (chunk) => {
-        //         console.log(`BODY: ${chunk}`);
-        //     });
-        //     res.on('readable', () => {
-        //         req.pipe(outFile)
-        //     })
-        //     res.on('error', (e) => {
-        //         console.log(e.message);
-        //     })
-        //     res.on('end', () => {
-        //         console.log('No more data in response.');
-        //         // res.pipe(outFile)
-        //         // outFile.close()
-        //         win.loadURL(__dirname + '/rendertest.asar/index.html') //asarの中のアプリを開く
-        //     });
-        // })
-        // // エラーがあれば扱う。
-        // req.on('error', (e) => {
-        //     console.error(`problem with request: ${e.message}`);
-        // });
-        // req.end();
-        // req.pipe(outFile)
-
-        // // ファイルをダウンロードする//未パッケージ環境なら上手く動作した
-        request
-        .get(url)
-        .on('response', function (res) {
-            splashWin = createSplash() //ローディング画面開く
-            console.log('statusCode: ', res.statusCode);
-            console.log('content-length: ', res.headers['content-length']);
-        })
-        .on('complete',(d)=>{
-            splashWin.close() //ローディング画面閉じる
-            outFile.close()
-            win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
-        })
-        .on('error',(e)=>{
-            console.log('Error:',e); return
-        })
-        .pipe(outFile);
-    // }else{
-    //     // win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
-    //     win.loadURL(__dirname + '/render.asar/index.html') //asarの中のアプリを開く
-    // }
 }
 
+/**
+ * ダウンロード中のスカッシュウィンドウ表示
+ * @returns window
+ */
 const createSplash = () => {
     const splash = new BrowserWindow({
         width: 1042,
@@ -97,6 +47,86 @@ const createSplash = () => {
     splash.loadURL(__dirname + '/loading.html')
 
     return splash
+}
+
+/**
+ * asarのダウンロード・置き換え
+ */
+const asarDownLoad = () => {
+    // if (app.isPackaged) {//アプリがパッケージングされてる場合
+        // ここで開く前にレンダラーのasarをサーバーから差し替え
+        // URLを指定 
+        const url = 'https://drive.google.com/uc?id=1mHfOd4seMjFuknv6hB1C55U9kMiw64sN&confirm=t';
+        // const url = 'https://techcrunch.com/wp-content/uploads/2022/04/GettyImages-1240090042.jpg?w=1390&crop=1';
+        
+        // 出力ファイル名を指定
+        const outFile = originalfs.createWriteStream(app.getPath('userData') + '/render.asar');
+        let splashWin
+        // const outFile = originalfs.createWriteStream('./rendertest.asar');
+        // const outFile = originalfs.createWriteStream('./image.jpg');
+
+        // request使わずにダウンロード
+
+        // const download = (uri, filename) => {
+        //     return new Promise((resolve, reject) =>
+        //       https
+        //         .request(uri, (res) => {
+        //           res
+        //             .pipe(filename)
+        //             .on("close", resolve)
+        //             .on("error", reject);
+        //         })
+        //         .end()
+        //     );
+        //   };
+          
+        //   download(
+        //     url,
+        //     outFile
+        //   ).then(() => {
+        //       console.log("done")
+        //       win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
+        //     });
+          
+        // ダウンロード開始
+        // var req = https.get(url, function (res) {
+
+        //     // ダウンロードした内容をそのまま、ファイル書き出し。
+        //     res.pipe(outFile);
+
+        //     // 終わったらファイルストリームをクローズ。
+        //     res.on('end', function () {
+        //         outFile.close();
+        //     }); 
+        // });
+
+        // // エラーがあれば扱う。
+        // req.on('error', function (err) {
+        //     console.log('Error: ', err); return;
+        // });
+
+        // // ファイルをダウンロードする//未パッケージ環境なら上手く動作した
+        request
+        .get(url)
+        .on('response', function (res) {
+            splashWin = createSplash() //ローディング画面開く
+            console.log('statusCode: ', res.statusCode);
+            console.log('content-length: ', res.headers['content-length']);
+        })
+        .on('complete',(d)=>{
+            splashWin.close() //ローディング画面閉じる
+            outFile.close()
+            createWindow() 
+        })
+        .on('error',(e)=>{
+            console.log('Error:',e); return
+        })
+        .pipe(outFile);
+
+    // }else{
+    //     // win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
+    //     win.loadURL(__dirname + '/render.asar/index.html') //asarの中のアプリを開く
+    // }
 }
 
 //------------------------------------
@@ -132,7 +162,7 @@ if (app.isPackaged) {//アプリがパッケージングされてる場合
 }
 
 // 初期化が終了したらウィンドウをを新規に作成する
-app.whenReady().then(createWindow)
+app.whenReady().then(asarDownLoad)
 
 // 全てのウィンドウが閉じられた時の処理
 app.on('window-all-closed', () => {
