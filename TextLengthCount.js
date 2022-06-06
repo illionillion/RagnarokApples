ddbcc60cb18df81e5f1948652ac1fec71dbec4ba
yuto ichinose <yuto.0526.sano@gmail.com>
2022-06-07 08:51:48 +0900

    コマンドラインからでも文字列を引数として入力できるようにした。

--- TextLengthCount.js
+++ TextLengthCount.js
@@ -1,18 +1,30 @@
 "use strict";
 const readline = require('readline');
- 
-const readInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-readInterface.question("入力してください >",
+
+// コマンドライン引数を受け取れるようにした
+if(process.argv.slice(2).length > 0){
+    const inputString = process.argv.slice(2)[0]
+    console.log( `
+        入力された文字
+        >>>${inputString}
+        文字数
+        >>>${inputString.length}
+    `);
+}else{
+    
+    const readInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    readInterface.question("入力してください >",
     inputString=>{
         readInterface.close();
         console.log( `
-            入力された文字
-            >>>${inputString }
-            文字数
-            >>>${inputString.length}
+        入力された文字
+        >>>${inputString}
+        文字数
+        >>>${inputString.length}
         `);
     }
-);
+    );
+}
