fdc51d20b8f0d1885029fb8dc3c89004e1677662
yuto ichinose <yuto.0526.sano@gmail.com>
2022-06-10 23:26:53 +0900

    http/fsでのダウンロード処理完成

--- src/asarDownLoad.js
+++ src/asarDownLoad.js
@@ -1,10 +1,7 @@
 const { app } = require('electron')
-const http = require('http')
 const https = require('https')
 const originalfs = require('original-fs');
-const request = require('request');
 const { createWindow, createSplash } = require('./createWindow.js');
-// const fetch = require('node-fetch');
 
 /**
  * asarのダウンロード・置き換え
@@ -14,76 +11,61 @@ const asarDownLoad = () => {
         // ここで開く前にレンダラーのasarをサーバーから差し替え
         // URLを指定 
         const url = 'https://drive.google.com/uc?id=1mHfOd4seMjFuknv6hB1C55U9kMiw64sN&confirm=t';
-        const urlTest = 'http://or0e9abi5m.php.xdomain.jp/asartest/render.asar';
-        // const urlLocal = 'http://127.0.0.1:8080/render.asar'; //こっちならhttp行ける
-        const imageUrl = 'https://techcrunch.com/wp-content/uploads/2022/04/GettyImages-1240090042.jpg?w=1390&crop=1';
         
         // 出力ファイル名を指定
-        const outFile = originalfs.createWriteStream(app.getPath('userData') + '/render.asar');
-        let splashWin
-        const outTestFile = originalfs.createWriteStream('./rendertest.asar');
-        const outImageFile = originalfs.createWriteStream('./image.jpg');
+        const outURL = app.getPath('userData') + '/render.asar'
 
         // request使わずにダウンロード
-        /*
-        1.googledriveでDriveToWebでホスティングしている場合はrequestでできる
-        2.googledriveでなかったらhttp/fsでできる
-        ただし、無料レンタルサーバーだとasarをアップロードできない
-        */
-        // ダウンロード開始
-        const req = https.get(url, function (res) {
-            // splashWin = createSplash() //ローディング画面開く
+       
+        // // ダウンロード開始
 
-            console.log(res.statusCode); // 303が返ってくる
-            console.log(res.statusMessage);
-            console.log(res.headers.location);
-
-            // 303だった場合locationを見てそこから取得
-            // ダウンロードした内容をそのまま、ファイル書き出し。
-            res.pipe(outTestFile);
-            // res.pipe(outFile);
-
-            // 終わったらファイルストリームをクローズ。
-            res.on('end', function () {
-                outTestFile.close();
-                // outFile.close();
-                // createWindow()
-                // setTimeout(() => {
-                //     splashWin.close() //ローディング画面閉じる
-                // }, 2000);
-            }); 
-        });
-
-        // エラーがあれば扱う。
-        req.on('error', function (err) {
-            console.log('Error: ', err); return;
-        });
-
-        // // ファイルをダウンロードする
-        request
-        .get(url)
-        .on('response', function (res) {
-            splashWin = createSplash() //ローディング画面開く
-            console.log('statusCode: ', res.statusCode);
-            console.log('content-length: ', res.headers['content-length']);
-        })
-        .on('complete',()=>{
-            outFile.close()
-            console.log('ended');
-            createWindow()
-            setTimeout(() => {
-                splashWin.close() //ローディング画面閉じる
-            }, 2000);
-        })
-        .on('error',(e)=>{
-            console.log('Error:',e); return
-        })
-        .pipe(outFile);
+        get(url, outURL)
 
     // }else{
     //     // win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
     //     win.loadURL(__dirname + '/render.asar/index.html') //asarの中のアプリを開く
     // }
+}
+
+/**
+ * http.get
+ * @param {*} url ダウンロードするファイルのURL
+ * @param {*} outURL 出力するファイルのURL
+ */
+const get = (url, outURL) => {
+    const req = https.get(url, (res) => {
+        let splashWin = createSplash() //ローディング画面開く
+
+        console.log(res.statusCode); // 303が返ってくる
+        console.log(res.statusMessage);
+
+        // 303だった場合locationを見てそこから取得
+        if (res.statusCode === 303) {
+            get(res.headers.location, outURL) // 再帰
+            return
+        }
+        // ダウンロードした内容をそのまま、ファイル書き出し。
+        const outFile = originalfs.createWriteStream(outURL);
+        res.pipe(outFile);
+
+        // 終わったらファイルストリームをクローズ。
+        res.on('end', function () {
+            console.log('end');
+            outFile.close();
+            createWindow()
+            setTimeout(() => {
+                splashWin.close() //ローディング画面閉じる
+            }, 2000);
+            return
+        }); 
+    });
+
+    // エラーがあれば扱う。
+    req.on('error', function (err) {
+        console.log('Error: ', err);
+        return;
+    });
+
 }
 
 exports.asarDownLoad = asarDownLoad
