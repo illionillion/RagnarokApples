6124d778da0bcb7ac80c5ab156aee73872791cac
yuto ichinose <yuto.0526.sano@gmail.com>
2022-06-11 17:17:25 +0900

    非同期化できた #13

--- src/asarDownLoad.js
+++ src/asarDownLoad.js
@@ -1,71 +1,47 @@
-const { app } = require('electron')
 const https = require('https')
 const originalfs = require('original-fs');
-const { createWindow, createSplash } = require('./createWindow.js');
-
-/**
- * asarのダウンロード・置き換え
- */
-const asarDownLoad = () => {
-    // if (app.isPackaged) {//アプリがパッケージングされてる場合
-        // ここで開く前にレンダラーのasarをサーバーから差し替え
-        // URLを指定 
-        const url = 'https://drive.google.com/uc?id=1mHfOd4seMjFuknv6hB1C55U9kMiw64sN&confirm=t';
-        
-        // 出力ファイル名を指定
-        const outURL = app.getPath('userData') + '/render.asar'
-
-        // request使わずにダウンロード
-       
-        // // ダウンロード開始
-        get(url, outURL)
-
-    // }else{
-    //     // win.loadURL(app.getPath('userData') + '/render.asar/index.html') //asarの中のアプリを開く
-    //     win.loadURL(__dirname + '/render.asar/index.html') //asarの中のアプリを開く
-    // }
-}
 
 /**
  * http.get
+ * asarのダウンロード・置き換え
  * @param {*} url ダウンロードするファイルのURL
  * @param {*} outURL 出力するファイルのURL
  */
-// const get = (url, outURL, splashWin) => {
-let splashWin
-const get = (url, outURL) => {
-    const req = https.get(url, (res) => {
-        
-        splashWin = !splashWin || createSplash() //ローディング画面開く
-        console.log(res.statusCode); // 303が返ってくる
-        console.log(res.statusMessage);
-        
-        // 303だった場合locationを見てそこから取得
-        if (res.statusCode === 303) {
-            get(res.headers.location, outURL) // 再帰
-            return
-        }
-        // ダウンロードした内容をそのまま、ファイル書き出し。
-        const outFile = originalfs.createWriteStream(outURL);
-        res.pipe(outFile);
+const asarDownLoad = (url, outURL) => {
 
-        // 終わったらファイルストリームをクローズ。
-        res.on('end', function () {
-            console.log('end');
-            outFile.close();
-            createWindow()
-            setTimeout(() => {
-                splashWin.close() //ローディング画面閉じる
-            }, 2000);
-            return
-        }); 
-    });
+    return new Promise((resolve, reject) => {
 
-    // エラーがあれば扱う。
-    req.on('error', function (err) {
-        console.log('Error: ', err);
-        return;
-    });
+        const req = https.get(url, async (res) => {
+            
+            console.log(res.statusCode); // 303が返ってくる
+            console.log(res.statusMessage);
+            
+            // 303だった場合locationを見てそこから取得
+            if (res.statusCode === 303) {
+                await asarDownLoad(res.headers.location, outURL) // 再帰
+                resolve()
+                return
+            }
+            // ダウンロードした内容をそのまま、ファイル書き出し。
+            const outFile = originalfs.createWriteStream(outURL);
+            res.pipe(outFile);
+    
+            // 終わったらファイルストリームをクローズ。
+            res.on('end', function () {
+                console.log('end');
+                outFile.close();
+                resolve()
+                return
+            }); 
+        });
+    
+        // エラーがあれば扱う。
+        req.on('error', function (err) {
+            console.log('Error: ', err);
+            reject()
+            return;
+        });
+    })
 
 }
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 // モジュールロード
 const { app, Menu, BrowserWindow } = require('electron')
 const { asarDownLoad } = require('./asarDownLoad.js')
+const { createWindow, createSplash } = require('./createWindow.js');
+
+/**
+ * メインプロセス
+ */
+const mainProcess = async () => {
+    // URLを指定 
+    const url = 'https://drive.google.com/uc?id=1mHfOd4seMjFuknv6hB1C55U9kMiw64sN&confirm=t';
+    // 出力ファイル名を指定
+    const outURL = app.getPath('userData') + '/render.asar'
+
+    // request使わずにダウンロード
+    let splashWin = createSplash()
+    await asarDownLoad(url, outURL)
+    createWindow()
+    setTimeout(() => {
+        splashWin.close() //ローディング画面閉じる
+    }, 2000);
+}
 
 //------------------------------------
 // メニュー
@@ -35,7 +54,7 @@ if (app.isPackaged) {//アプリがパッケージングされてる場合
 }
 
 // 初期化が終了したらウィンドウをを新規に作成する
-app.whenReady().then(asarDownLoad)
+app.whenReady().then(mainProcess)
 
 // 全てのウィンドウが閉じられた時の処理
 app.on('window-all-closed', () => {
@@ -51,6 +70,6 @@ app.on('activate', () => {
     // ウィンドウがすべて閉じられている場合は新しく開く
     if (BrowserWindow.getAllWindows().length === 0) {
         // Macにて信号赤が押されてからアイコンクリックされた時にダウンロードから始める
-        asarDownLoad()
+        createWindow()
     }
 })
