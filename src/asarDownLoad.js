cd7a11474c5aa664547a86053831a472f8cf24b0
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-06-12 09:06:40 +0900

    セミコロン削除、コメントアウト追加

--- src/asarDownLoad.js
+++ src/asarDownLoad.js
@@ -1,5 +1,5 @@
 const https = require('https')
-const originalfs = require('original-fs');
+const originalfs = require('original-fs')
 
 /**
  * http.get
@@ -14,36 +14,36 @@ const asarDownLoad = async (url, outURL) => {
 
             const req = https.get(url, async (res) => {
 
-                console.log(res.statusCode); // 303が返ってくる
-                console.log(res.statusMessage);
+                console.log(res.statusCode) // 303が返ってくる
+                console.log(res.statusMessage)
 
                 // 303だった場合locationを見てそこから取得
                 if (res.statusCode === 303) {
-                    await asarDownLoad(res.headers.location, outURL); // 再帰
-                    resolve(true);
-                    return;
+                    await asarDownLoad(res.headers.location, outURL) // 再帰
+                    resolve(true) //trueを返す
+                    return
                 }
                 // ダウンロードした内容をそのまま、ファイル書き出し。
-                const outFile = originalfs.createWriteStream(outURL);
-                res.pipe(outFile);
+                const outFile = originalfs.createWriteStream(outURL)
+                res.pipe(outFile)
 
                 // 終わったらファイルストリームをクローズ。
                 res.on('end', () => {
-                    console.log('end');
-                    outFile.close();
-                    resolve(true);
-                });
-            });
+                    console.log('end')
+                    outFile.close()
+                    resolve(true) // trueを返す
+                })
+            })
 
             // エラーがあれば扱う。
             req.on('error', (err) => {
-                console.log('Error: ', err);
-                reject(false);
-            });
-        });
-    } catch (err_1) {
-        console.log(err_1);
-        return err_1;
+                console.log('Error: ', err)
+                reject(false) // falseを返す
+            })
+        })
+    } catch (err) { // rejectのfalseをキャッチ
+        console.log(err)
+        return err // falseを返す
     }
 
 }
