cbabd86cd3a609fa8431ae6d52f99069d3f33e3c
yuto ichinose <yuto.0526.sano@gmail.com>
2022-06-13 15:08:12 +0900

    completedの表示

--- src/loading.js
+++ src/loading.js
@@ -7,6 +7,7 @@ window.onload = () => {
     ipcRenderer.on('loadingData', (event, arg) => {//受け取り
         document.getElementById('percent').innerText = `${arg} %`
         document.getElementById('loading').value = arg
+        if(arg == 100) document.querySelector('label>h1').innerText = 'Completed!'
     })
 
 } 
