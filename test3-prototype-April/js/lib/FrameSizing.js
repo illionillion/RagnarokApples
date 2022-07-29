15beabfe98a3c005cea01d21d1d70bfc958b4228
yuto ichinose <yuto.0526.sano@gmail.com>
2022-07-29 13:56:54 +0900

    山本くんに割り当て予定 #28

--- test3-prototype-April/js/lib/FrameSizing.js
+++ test3-prototype-April/js/lib/FrameSizing.js
@@ -7,14 +7,17 @@ export default function FrameSizing(){
     let ScreenWidth = document.documentElement.clientWidth
     let ScreenHeight = 9 * ScreenWidth / 16 
     const windowHeight = document.documentElement.clientHeight
-    console.log(ScreenWidth);
-    console.log(ScreenHeight);
-    console.log(windowHeight);
+    
     if (ScreenHeight > windowHeight) {
 
         ScreenHeight = windowHeight
-        ScreenWidth = 16 * ScreenHeight / 9 
+        ScreenWidth = 16 * ScreenHeight / 9
+
     }
+
+    // ここで比率を出したい // 2つの値の最大公約数を求めてそれで割る
+    console.log(`${ScreenWidth} : ${ScreenHeight}`);
+    
     document.getElementById('frame').style.width = `${ScreenWidth}px`
     document.getElementById('frame').style.height = `${ScreenHeight}px`
 }
