f7ef20376fabbd78a04ab3b0f88e91d593975c18
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-07-30 13:20:33 +0900

    横幅に黒画面ができる時に、アスペクト比が16:9じゃなくなる

--- test3-prototype-April/js/lib/FrameSizing.js
+++ test3-prototype-April/js/lib/FrameSizing.js
@@ -1,23 +1,33 @@
-export default function FrameSizing(){
-    // console.log('size');
+export default function FrameSizing() {
+  // console.log('size');
 
-    //PC画面の幅を取得してから16:9で計算
-    //縦のサイズを割り当てる
+  //PC画面の幅を取得してから16:9で計算
+  //縦のサイズを割り当てる
 
-    let ScreenWidth = document.documentElement.clientWidth
-    let ScreenHeight = 9 * ScreenWidth / 16 
-    const windowHeight = document.documentElement.clientHeight
-    
-    if (ScreenHeight > windowHeight) {
+  let ScreenWidth = document.documentElement.clientWidth;
+  let ScreenHeight = (9 * ScreenWidth) / 16;
+  const windowHeight = document.documentElement.clientHeight;
+  let g = gcd(ScreenHeight, ScreenWidth);
 
-        ScreenHeight = windowHeight
-        ScreenWidth = 16 * ScreenHeight / 9
+  if (ScreenHeight > windowHeight) {
+    ScreenHeight = windowHeight;
+    ScreenWidth = (16 * ScreenHeight) / 9;
+    g = gcd(ScreenWidth, ScreenHeight);
+    console.log("hoge");
+  }
 
-    }
+  // 最大公約数を求める
+  function gcd(w, h) {
+    if (h === 0) return w;
+    return gcd(h, w % h);
+  }
 
-    // ここで比率を出したい // 2つの値の最大公約数を求めてそれで割る
-    console.log(`${ScreenWidth} : ${ScreenHeight}`);
-    
-    document.getElementById('frame').style.width = `${ScreenWidth}px`
-    document.getElementById('frame').style.height = `${ScreenHeight}px`
+  
+
+  // ここで比率を出したい // 2つの値の最大公約数を求めてそれで割る
+  // console.log(`${ScreenWidth} : ${ScreenHeight}`);
+  console.log(`${ScreenWidth / g} : ${ScreenHeight / g}`);
+
+  document.getElementById("frame").style.width = `${ScreenWidth}px`;
+  document.getElementById("frame").style.height = `${ScreenHeight}px`;
 }
