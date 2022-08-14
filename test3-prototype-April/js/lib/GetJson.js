1cc76bd0257cafd60f0ce4741a1e67f8e0ca84bc
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-14 18:00:01 +0900

    ゲーム側でプリロードスクリプト使う準備完了、次回からPSやる

--- test3-prototype-April/js/lib/GetJson.js
+++ test3-prototype-April/js/lib/GetJson.js
@@ -4,6 +4,6 @@
  */
 export async function GetScenarioJson() {
     const req = await fetch('https://ji9xputuw8gwgczk2gnxzg.on.drv.tw/www.render.asar.server.com/scenario_data.json')
-    console.log('Get JSON');
+    console.log('Get JSON!');
     return await req.json()
 }
