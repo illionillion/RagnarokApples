c7530f88e9cccd268758312a36764a6e29c71c9a
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-17 08:57:16 +0900

    イベントリステナーを修正 #15

--- src/loading.js
+++ src/loading.js
@@ -1,13 +1,15 @@
 const { ipcRenderer } = require('electron')
 // console.log('ok');
+window.addEventListener('load', () => {
 
-window.onload = () => {
-    document.body.oncontextmenu = () => {return false}
-    
+    document.addEventListener('contextmenu', () => {return false})
+
     ipcRenderer.on('loadingData', (event, arg) => {//受け取り
+
         document.getElementById('percent').innerText = `${arg} %`
         document.getElementById('loading').value = arg
         if(arg == 100) document.querySelector('label>h1').innerText = 'Completed!'
+        
     })
 
-} 
+})
