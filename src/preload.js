0e29df350fbcad8938f6ee0c7743933069f1d8be
yuto ichinose <yuto.0526.sano@gmail.com>
2023-05-20 13:48:03 +0900

    #103 イメージマップのリサイズ導入

--- src/preload.js
+++ src/preload.js
@@ -1,22 +1,26 @@
-const { contextBridge, ipcRenderer, app } = require('electron');
+const { contextBridge, ipcRenderer, app } = require("electron");
+const resize = require("image-map-resizer");
 // const { writeJson } = require('./writeFile');
+console.log('resize', resize);
 
-require('dotenv').config({ path: __dirname + '/../.env' }) // .env読み込み
+require("dotenv").config({ path: __dirname + "/../.env" }); // .env読み込み
 
-contextBridge.exposeInMainWorld('myAPI', {
-    // .envの情報をプリロードスクリプトで読み込ませる
-    URLS: {
-        SCENARIO_DATA_JSON: process.env.SCENARIO_DATA_JSON,
-        SCENARIO_AUDIO_DATA_JSON: process.env.SCENARIO_AUDIO_DATA_JSON,
-        SCENARIO_MAP_DATA_JSON: process.env.SCENARIO_MAP_DATA_JSON,
-        SCENARIO_ASSETS_DATA_JSON: process.env.SCENARIO_ASSETS_DATA_JSON
-    }, 
-    isPackaged: async () => await ipcRenderer.invoke('isPackaged'), // パッケージ状態か確認
-    writeJson: async (data) => await ipcRenderer.invoke('writeJson', data),
-    getAssetsPath: async () => await ipcRenderer.invoke('getAssetsPath'),
-    on: (channel, callback) => ipcRenderer.on(channel, (event, argv) => callback(event, argv)), // メイン → レンダラー
-    LOCAL_BUILD_DIR: process.env.LOCAL_BUILD_DIR,
-    __dirname: __dirname,
-})
+contextBridge.exposeInMainWorld("myAPI", {
+  // .envの情報をプリロードスクリプトで読み込ませる
+  URLS: {
+    SCENARIO_DATA_JSON: process.env.SCENARIO_DATA_JSON,
+    SCENARIO_AUDIO_DATA_JSON: process.env.SCENARIO_AUDIO_DATA_JSON,
+    SCENARIO_MAP_DATA_JSON: process.env.SCENARIO_MAP_DATA_JSON,
+    SCENARIO_ASSETS_DATA_JSON: process.env.SCENARIO_ASSETS_DATA_JSON,
+  },
+  isPackaged: async () => await ipcRenderer.invoke("isPackaged"), // パッケージ状態か確認
+  writeJson: async (data) => await ipcRenderer.invoke("writeJson", data),
+  getAssetsPath: async () => await ipcRenderer.invoke("getAssetsPath"),
+  on: (channel, callback) =>
+    ipcRenderer.on(channel, (event, argv) => callback(event, argv)), // メイン → レンダラー
+  LOCAL_BUILD_DIR: process.env.LOCAL_BUILD_DIR,
+  __dirname: __dirname,
+  imageMapResize: resize,
+});
 
-console.log('preload!!');
+console.log("preload!!");
